9a3acc939d0a93da4fb0a006540f6d8aa5a04387
joelhuba <joelhuba1999@gmail.com>
2025-10-05 13:50:37 -0500

    modificacion en el la version de angular

--- src/app/presentation/account/login/login.component.ts
+++ src/app/presentation/account/login/login.component.ts
@@ -1,12 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { AuthUseCase } from '../../../infrastructure/use-case/auth/auth.use-case';
+import { JwtDecoderHelper } from '../../../infrastructure/helpers/decodec-token.helper';
+
 
 @Component({
-  selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule
+  ],
+  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+
+  loginForm: UntypedFormGroup | undefined;
+  submitted: any = false;
+  error: any = '';
+  returnUrl: string | undefined;
+  fieldTextType!: boolean;
+
+  year: number = new Date().getFullYear();
+
+  loginFailed = false;
+  loading = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private _authUseCase: AuthUseCase,
+    private router: Router,
+    private jwtHelper: JwtDecoderHelper
+  ) { }
+
+  ngOnInit() {
+    this.initForm();
+
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      this.router.navigate(['/parametrization/patients']); // lo manda a la página principal
+    }
+  }
+
+  private initForm(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get f() { return this.loginForm?.controls ?? {}; }
+
+    onSubmit() {
+    this.submitted = true;
+    this.loginFailed = false;
+
+    if (this.loginForm && this.loginForm.valid) {
+      this.loading = true;
+
+      const { userName, password } = this.loginForm.value;
+
+      this._authUseCase.login(userName, password).subscribe({
+        next: (response: any) => {
+          this.loading = false;
+
+          const token = localStorage.getItem('authToken');
+
+          if (token) {
+            const decoded = this.jwtHelper.getDecodedAccessToken(token);
+
+            if (decoded && decoded.IdUser && decoded.IdCompany && decoded.IdRol) {
+              localStorage.setItem('IdUser', decoded.IdUser);
+              localStorage.setItem('IdCompany', decoded.IdCompany);
+              localStorage.setItem('IdRol', decoded.IdRol);
+            }
+
+            this.router.navigate(['/parametrization/patients']);
+          } else {
+            this.loginFailed = true;
+          }
+        },
+        error: err => {
+          this.loading = false;
+          this.loginFailed = true;
+          console.error('Error al iniciar sesión', err);
+        }
+      });
+    }
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
 }
